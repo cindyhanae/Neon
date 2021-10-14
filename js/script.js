b01c2066856280af90278814ae1a7e1785ebd98b
cindyhanae <cinde01@gmail.com>
2021-10-14 14:50:01 -0300

    fixed menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// DROPDOWN
 const btnDropdown = document.querySelector('.js-btn-dropdown')
 const dropdown = document.querySelector('.js-dropdown')
 
@@ -12,4 +13,25 @@ btnDropdown.firstElementChild.addEventListener('click', openDropdown)
 
 // fecha dropdown quando mouse deixa área
 dropdown.addEventListener('mouseleave', openDropdown)
+
+
+
+// MENU FIXO
+const headerSite = document.querySelector('#js-header')
+
+function fixedMenu(){
+  // preciso pegar a posição de onde está o scroll da página e adicionar uma classe ao menu
+  // .pageYOffset é uma propriedade do window que retorna a posição do scroll em px.
+  if (window.pageYOffset > 80) {
+    headerSite.classList.add('fixed-menu')
+
+  } else {
+    headerSite.classList.remove('fixed-menu')
+
+  }
+}
+
+// o evento precisa ser adicionado ao window
+window.addEventListener('scroll', fixedMenu)
+
 
